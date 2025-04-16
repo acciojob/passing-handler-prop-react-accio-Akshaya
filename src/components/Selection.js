65a1b3256f600f824dc3aa51a20521462194ad4e
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-04-16 00:49:43 -0700

    Initial commit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,22 @@
+import React, { useState } from "react";
 
+const Selection = (props) => {
+    const [BgStyle, setBgStyle] = useState({background:"" });
+
+    const handleClick = () =>{
+        props.applyColor(setBgStyle);
+    };
+
+    return(
+        <div 
+           className="fix-box"
+           style={BgStyle}
+           onClick={handleClick}
+        >
+         Selection
+         
+        </div>
+    );
+};
+
+export default Selection;
